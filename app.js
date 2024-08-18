e1514a6f5768f0cc5c91000ae6c49dbaee60e37c
Shahbaz Ahmed <ashabaz845@gmail.com>
2024-08-18 13:13:20 +0500

    completed Task

---
+++ app.js
@@ -0,0 +1,73 @@
+const showHandler  = () =>{
+    let para = document.getElementById("para")
+    let plus = document.getElementById("plus")
+    let minus = document.getElementById("minus")
+
+    
+
+    para.style.display = "block"
+    plus.style.display="none"
+    minus.style.display= "block"
+}
+
+const hiddenHandler = () =>{
+    let para = document.getElementById("para")
+    let plus = document.getElementById("plus")
+    let minus = document.getElementById("minus")
+
+
+    para.style.display = "none"
+    plus.style.display="block"
+    minus.style.display= "none"
+
+}
+
+const showHandler1  = () =>{
+    let para1 = document.getElementById("para1")
+    let plus1 = document.getElementById("plus1")
+    let minus1 = document.getElementById("minus1")
+
+    
+
+    para1.style.display = "block"
+    plus1.style.display="none"
+    minus1.style.display= "block"
+}
+
+const hiddenHandler1 = () =>{
+    let para1 = document.getElementById("para1")
+    let plus1 = document.getElementById("plus1")
+    let minus1 = document.getElementById("minus1")
+
+
+    para1.style.display = "none"
+    plus1.style.display="block"
+    minus1.style.display= "none"
+
+}
+
+const showHandler2  = () =>{
+    let para2 = document.getElementById("para2")
+    let minus2 = document.getElementById("minus2")
+    let plus2 = document.getElementById("plus2")
+
+    
+
+    para2.style.display = "block"
+    plus2.style.display="none"
+    minus2.style.display= "block"
+}
+
+const hiddenHandler2 = () =>{
+    let para2 = document.getElementById("para2")
+    let plus2 = document.getElementById("plus2")
+    let minus2 = document.getElementById("minus2")
+
+
+    para2.style.display = "none"
+    plus2.style.display="block"
+    minus2.style.display= "none"
+
+}
+
+
